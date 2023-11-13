8b2e3c0033ede42e5e7e2a87dae4d66fc734008b
Marcelo de Moura Cavalcanti Junior <marcelo.cavalcanti@mv.com.br>
2023-11-13 16:52:06 -0300

    BugFix CORS

--- API.Deploy/src/main/java/com/br/marcelo/API/Deploy/Application.java
+++ API.Deploy/src/main/java/com/br/marcelo/API/Deploy/Application.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@OpenAPIDefinition(servers = {@Server(url = "/",description = "Default server URL")})
+//@OpenAPIDefinition(servers = {@Server(url = "/",description = "Default server URL")})
 @SpringBootApplication
 public class Application {
 
